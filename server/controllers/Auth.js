bd88b53871f7ad8c1666d5ce317513f8c0bcb9f3
Ashish55js <87162293+Ashish55js@users.noreply.github.com>
2023-10-22 21:26:24 +0530

    Update Auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -191,7 +191,7 @@ exports.sendotp = async (req, res) => {
 		// If user found with provided email
 		if (checkUserPresent) {
 			// Return 401 Unauthorized status code with error message
-			res.status(401).json({
+			return res.status(401).json({
 				success: false,
 				message: `User is Already Registered`,
 			});
